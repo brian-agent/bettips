ac2490ccdc2261a8da33b7029d50a4aabba1216b
Brian-Agent <kipronobrian99@gmail.com>
2025-01-07 08:29:53 +0300

    Add endpoints for yesterday and previous days' games

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,11 @@ from . import views
 urlpatterns= [
 
     path('',views.home,name='home'),
-    path('all_games',views.all_games,name='all_games')
+    path('all_games',views.all_games,name='all_games'),
+    path('yesterday_games',views.yesterday_games,name='yesterday_games'),
+    path('previous_two_days_games',views.previous_two_days_games,name='previous_two_days_games'),
+    path('previous_three_days_games',views.previous_three_days_games,name='previous_three_days_games'),
+    path('previous_four_days_games',views.previous_four_days_games,name='previous_four_days_games'),
 
 
 ]
--- api/views.py
+++ api/views.py
@@ -6,6 +6,7 @@ from .serializer import FixtureSerializer
 from rest_framework.decorators import api_view
 from rest_framework import status
 from datetime import date
+from datetime import timedelta
 
 # Create your views here.
 @api_view(['GET'])
@@ -18,5 +19,26 @@ def home(request):
 @api_view(['GET'])
 def all_games(request):
     fixtures=Fixture.objects.all()
-    serializer= FixtureSerializer(fixtures)
+    serializer= FixtureSerializer(fixtures, many=True)
     return Response(serializer.data,status=status.HTTP_200_OK)
+def yesterday_games(request):
+    yesterday = date.today() - timedelta(days=1)
+    fixtures = Fixture.objects.filter(created_at__date=yesterday)
+    serializer = FixtureSerializer(fixtures, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+def previous_two_days_games(request):
+    two_days_ago = date.today() - timedelta(days=2)
+    fixtures = Fixture.objects.filter(created_at__date=two_days_ago)
+    serializer = FixtureSerializer(fixtures, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+def previous_three_days_games(request):
+    three_days_ago = date.today() - timedelta(days=3)
+    fixtures = Fixture.objects.filter(created_at__date=three_days_ago)
+    serializer = FixtureSerializer(fixtures, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+def previous_four_days_games(request):
+    four_days_ago = date.today() - timedelta(days=4)
+    fixtures = Fixture.objects.filter(created_at__date=four_days_ago)
+    serializer = FixtureSerializer(fixtures, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
